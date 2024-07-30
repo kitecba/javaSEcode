ec8c28e0fc429c0e13341e44412a07364b38ec21
wsjstart <468676419@qq.com>
2024-07-30 22:38:57 +0800

    third commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloword");
         System.out.println("wsj");
+        System.out.println("wei");
     }
 }
 
